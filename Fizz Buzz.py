9d565f3902724738e6c35c8d0898ba2f6c42436c
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2022-09-17 05:51:48 -0700

    Update Fizz Buzz.py

--- Fizz Buzz.py
+++ Fizz Buzz.py
@@ -3,14 +3,11 @@ class Solution(object):
         list1 = []
         for i in range(1, n + 1):
             list1.append(str(i))
-            if i%3 == 0 and i%5 == 0 :
-                list1.remove(str(i))
+            if i%3 == 0 and i%5 == 0:
                 list1.append("FizzBuzz")
             elif i%3 == 0:
-                list1.remove(str(i))
                 list1.append("Fizz")
             elif i%5 == 0:
-                list1.remove(str(i))
                 list1.append("Buzz")
                 
         return list1
