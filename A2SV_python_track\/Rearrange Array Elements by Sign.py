a304971ab6e3fe89ef8824b222c0e50bdfb1dd47
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-01-09 10:30:50 +0300

    Rearrange Array Elements by Sign

--- A2SV_python_track\/Rearrange Array Elements by Sign.py
+++ A2SV_python_track\/Rearrange Array Elements by Sign.py
@@ -1,18 +1,18 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         
-        ans = [0]*len(nums)
-        i = 0
-        j = 1
+        rearranged_nums = [0]*len(nums)
+        positive_nums_index = 0
+        negative_nums_index = 1
 
-        for s in range(len(nums)):
+        for index in range(len(nums)):
 
-            if nums[s] > 0:
-                ans[i] = nums[s]
-                i += 2
+            if nums[index] > 0:
+                rearranged_nums[positive_nums_index] = nums[index]
+                positive_nums_index += 2
 
-            if nums[s] < 0:
-                ans[j] = nums[s]
-                j += 2
+            else:
+                rearranged_nums[negative_nums_index] = nums[index]
+                negative_nums_index += 2
                 
-        return ans
+        return rearranged_nums
