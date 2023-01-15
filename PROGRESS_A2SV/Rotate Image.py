600da5c61d759fe13a6044696b367dcb9d03f8f1
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-01-15 15:14:30 +0300

    Rotate Image

---
+++ PROGRESS_A2SV/Rotate Image.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        matrix.reverse()
+        
+        for row in range(len(matrix)):
+            for column in range(row):
+
+                matrix[row][column], matrix[column][row] = matrix[column][row], matrix[row][column]
+                
+        return matrix
