aea92b77b32c125658bca004f8e9aded7e8c6ce8
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-01-17 23:49:37 +0300

    Toeplitz Matrix

---
+++ PROGRESS_A2SV/Toeplitz Matrix/Toeplitz Matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+
+        row = len(matrix)
+        col = len(matrix[0])
+        row_indx = 0
+
+        while row_indx < (row-1):
+
+            for col_indx in range(col-1):
+                if matrix[row_indx][col_indx] != matrix[row_indx+1][col_indx+1]:
+                    return False
+
+            row_indx += 1
+        
+        return True
