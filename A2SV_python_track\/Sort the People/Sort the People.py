55baa1cc0f78b6d8778c2939f1647eda9c70ce2c
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-01-18 11:17:10 +0300

    Sort the People

---
+++ A2SV_python_track\/Sort the People/Sort the People.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        
+        for i in range(len(names)):
+            for j in range(i+1):
+                
+                if heights[i] > heights[j]:
+    
+                    names[i], names[j] = names[j], names[i]
+                    heights[i], heights[j] = heights[j], heights[i]
+        
+        return names
