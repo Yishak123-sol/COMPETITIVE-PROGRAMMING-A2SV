b2bbb3d9d4ed31b4066b51e1ef24a81e24a2676a
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-01-19 22:25:15 +0300

    Sort Colors

---
+++ PROGRESS_A2SV/Sort Colors/Sort Colors.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        count_zero = 0
+        count_one = 0
+        count_two = 0
+        
+        for num in nums:
+            
+            if num == 0:
+                count_zero += 1
+            
+            elif num == 1:
+                count_one += 1
+            
+            else:
+                count_two += 1
+        
+        for index in range(len(nums)):
+
+            if count_zero > 0:
+                nums[index] = 0
+                count_zero -= 1
+            
+            elif count_one > 0:
+                nums[index] = 1
+                count_one -= 1
+
+            else:
+                nums[index] = 2
