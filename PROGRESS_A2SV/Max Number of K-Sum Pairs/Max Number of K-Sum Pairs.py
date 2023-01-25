19ae56b08e57fd1814d64597a50e16e5f82320e3
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-01-25 21:10:44 +0300

    Max Number of K-Sum Pairs

---
+++ PROGRESS_A2SV/Max Number of K-Sum Pairs/Max Number of K-Sum Pairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+
+        nums.sort()
+        left, right = 0, len(nums)-1
+        count = 0
+
+        while left < right:
+            if nums[left] + nums[right] == k:
+                left += 1
+                right -= 1
+                count += 1
+
+            elif nums[left] + nums[right] > k:
+                right -= 1
+
+            elif nums[left] + nums[right] < k:
+                left += 1
+                
+        return count
+                
