d206dd0b256a24ec4d49bd5da73e4de5ce5b3210
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-02-26 01:16:14 +0300

    Product of Array Except Self

---
+++ PROGRESS_A2SV/Product of Array Except Self/Product of Array Except Self.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        
+        output = [1]*len(nums)
+        left = 1
+        for i in range(1, len(nums)):
+            left *= nums[i-1]
+            output[i] = left
+            
+        right = 1
+        for i in range(len(nums)-2, -1, -1):
+            right *= nums[i+1]
+            output[i] *= right
+            
+        return output
