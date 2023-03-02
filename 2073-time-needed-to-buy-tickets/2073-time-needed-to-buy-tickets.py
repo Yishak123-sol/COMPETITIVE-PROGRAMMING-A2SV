205154ab681812a0a08760f10bd6834a3477e60a
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-03-02 07:36:47 +0300

    Time: 38 ms (78.62%), Space: 13.8 MB (53.73%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,14 +1,33 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         
-        time_taken = 0
+        # time_taken = 0
         length = len(tickets)
-        for i in range(length*100+2):
+#         for i in range(length*100+2):
             
-            if tickets[k%length] == 0:
-                return time_taken
+#             if tickets[k%length] == 0:
+#                 return time_taken
 
-            if tickets[i%length] != 0:
-                tickets[i%length] -= 1 
-                time_taken += 1
-            
+#             if tickets[i%length] != 0:
+#                 tickets[i%length] -= 1 
+#                 time_taken += 1
+
+        sold_Tickets = 0
+        deductions = 0
+        
+        for i in range(length):
+            if tickets[i] >= tickets[k]:
+                sold_Tickets += tickets[k]
+            else:
+                sold_Tickets += tickets[i]
+                
+            if i > k and tickets[i] >= tickets[k]:
+                deductions += 1
+                
+        time_taken = sold_Tickets - deductions
+        
+        return time_taken
+    
+    
+    
+    
