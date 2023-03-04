90672a683ae94cadb6a21b62ca79d8b158d26e5a
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-03-04 04:10:14 +0300

    0852-peak-index-in-a-mountain-array

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -2,7 +2,6 @@ class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         
         low, high = 0, len(arr) - 1
-        
         while low <= high:
             mid = low + (high - low)//2
             if arr[mid] < arr[mid+1]:
