e211143172aecc6675c368069b8f056e89dee897
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-03-16 14:15:43 -0800

    Time: 77 ms (70.13%), Space: 46.9 MB (15.58%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.js
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.js
@@ -1,22 +1,30 @@
-class Solution:
-    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
-        
-        right = max(nums)
-        minval = float('inf')
-        left = 1
-        while left <= right:
-            
-            mid = left + (right-left)//2
-            total = 0
-            for i in range(len(nums)):
-                total += math.ceil(nums[i]/mid)
-                if total > threshold:
-                    break
-            if total > threshold:
-                left = mid + 1
-            else:
-                minval = min(minval, mid)
-                right = mid - 1
-                
-                
-        return minval
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+var smallestDivisor = function(nums, threshold) {
+    var right = Math.max(...nums);
+    var minval = right + 1;
+    var left = 1;
+    
+    while (left <= right){
+        var mid = left + Math.floor((right - left)/2);
+        var total = 0;
+        for (let i = 0; i < nums.length; i++){
+            var num = nums[i]/mid
+            total += Math.ceil(num)
+            if(total > threshold){
+                break;
+            }
+        }
+        if (total > threshold){
+            left = mid + 1
+        }else{
+            minval = Math.min(minval, mid);
+            console.log(total)
+            right = mid - 1;
+        }
+    }
+    return minval
+};
