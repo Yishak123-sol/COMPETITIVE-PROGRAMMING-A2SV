450f630e0b49a9461a589d39499db579b03193f9
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-03-16 13:03:44 -0800

    Time: 27 ms (88.95%), Space: 13.9 MB (52.39%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -1,23 +1,9 @@
 class Solution:
-    def __init__(self):
-        self.ans = 0
-        
     def kthGrammar(self, n: int, k: int) -> int:
-        if n == 1:
-                return 0
-            
+        if n == 1:return 0
+        parent = self.kthGrammar(n-1, math.ceil(k/2))
+        isKodd = (k%2 != 0)
+        if parent == 1:
+            return 1 if isKodd else 0
         else:
-            s = self.kthGrammar(n - 1, math.ceil(k/2))
-
-            # s = string[math.ceil(k/2)]
-            if k % 2 == 0 and s == 1:
-                return 0
-            elif k % 2 == 0 and s == 0:
-                return 1
-            elif k % 2 != 0 and s == 1:
-                return 1
-            elif k % 2 != 0 and s == 0:
-                return 0
-        
-        self.ans = self.kthGrammar(n, k)
-        return self.ans
+            return 0 if isKodd else 1 
