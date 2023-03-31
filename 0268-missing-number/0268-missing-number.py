054eeb97506cd4790ff96470c398a2696afeb696
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-03-30 22:54:28 -0800

    Time: 147 ms (33.88%), Space: 15.2 MB (28.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,16 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        i = 0
-        while i < len(nums):
-            idx = abs(nums[i])
-
-            if idx < len(nums) and idx != nums[idx]:
-                nums[i],nums[idx] = nums[idx],nums[i]
-            else:
-                i+=1
-        
-        for i in range(len(nums)):
-            if i != nums[i]:
-                return i
+        res = 0
+        length = len(nums)
+        for i in range(1, length+1):
+            res = (res^nums[i-1]^i)
             
-        return i+1
+        return res
