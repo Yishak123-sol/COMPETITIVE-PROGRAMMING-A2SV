ca3f41b030b13fbdce7d1a7dff53786372f76b3e
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-04-04 11:48:06 -0800

    Time: 150 ms (86.74%), Space: 16.7 MB (18.24%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.py
+++ 0147-insertion-sort-list/0147-insertion-sort-list.py
@@ -4,46 +4,29 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if not head or not head.next:return head
-        
-        left, right = self.split(head)
-        left_half = self.insertionSortList(left)
-        right_half = self.insertionSortList(right)
-        
-        return self.merge(left_half, right_half)
-    
-    def split(self, head):
-        
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+     def add_to_sorted_list(self, head, sorted_head, sorted_tail):
             
-        mid = slow.next
-        slow.next = None
-        
-        return head, mid
-    
-    def merge(self, left_half, right_half):
-        
-        dummy = ListNode(0)
-        curr = dummy
-        while left_half and right_half:
+        if sorted_head == sorted_tail or head.val >= sorted_tail.val:
+            sorted_tail.next = head
+            head.next = None
+            sorted_tail = head
             
-            if left_half.val < right_half.val:
-                curr.next = left_half
-                left_half = left_half.next
+        else:
+            while sorted_head.next and sorted_head.next.val <= head.val:
+                sorted_head = sorted_head.next
                 
-            else:
-                curr.next = right_half
-                right_half = right_half.next
-            curr =  curr.next
+            head.next = sorted_head.next
+            sorted_head.next = head
             
-        curr.next = right_half or left_half
-        
-        return dummy.next
-                
-                
-        
+        return sorted_tail
+
+     def insertionSortList(self, head):
+
+        sorted_head = ListNode(None)
+        sorted_tail = sorted_head
+        while head:
+            temp = head.next
+            sorted_tail = self.add_to_sorted_list(head, sorted_head, sorted_tail)
+            head = temp
+            
+        return sorted_head.next
