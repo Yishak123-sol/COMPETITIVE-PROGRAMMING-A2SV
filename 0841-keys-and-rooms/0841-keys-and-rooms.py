a8bc2f2b49c6bb8c6df9a2851b1c43327348b534
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-04-27 06:48:15 -0800

    Time: 92 ms (5.27%), Space: 17 MB (10.24%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -7,16 +7,23 @@ class Solution:
         
         for i in range(len(rooms)):
             graph[i].extend(rooms[i])
-        self.dfs(0, visited, graph)
+        
+        
+        q = deque()
+        q.append(0)
+        
+        while q:
+            length = len(q)
+            for _ in range(length):
+                curr_room = q.popleft()
+                if visited[curr_room] == False:
+                    q.extend(graph[curr_room])
+                    visited[curr_room] = True
+        
         for key in visited:
             if not key:
                 return False
             
         return True
-    def dfs(self, room, visited, graph):
-        
-        visited[room] = True
-        for key in graph[room]:
-            if not visited[key]:
-                self.dfs(key, visited, graph)
                 
+            
