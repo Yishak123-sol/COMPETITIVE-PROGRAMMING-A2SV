89ddba5a5a652c0f5e5ba4b08db1c8169c8cefbc
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-04-29 08:52:53 -0800

    Time: 109 ms (17.73%), Space: 18.2 MB (5.04%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,18 +1,17 @@
+from collections import deque
 class Solution:
-    
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         
-        stack = []
-        stack.append((0, [0]))
+        queue = deque()
+        queue.append((0, [0]))
         paths = []
-        
-        while stack:
+        while queue:
+            node, path = queue.popleft()
             
-            node, path = stack.pop()
-            if node == len(graph)-1:
+            if len(graph)-1 == node:
                 paths.append(path)
             else:
                 for neighbor in graph[node]:
-                    stack.append((neighbor, path+[neighbor]))
-                
+                    queue.append((neighbor, path + [neighbor]))
+                    
         return paths
