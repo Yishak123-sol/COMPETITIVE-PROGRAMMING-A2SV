2c8c9966c0c750dc8507029aee62d74caf503f8b
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-05-11 08:50:25 -0800

    Time: 2961 ms (11.99%), Space: 182.1 MB (51.15%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -2,31 +2,38 @@ class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
         
         direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        visited = set()
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if (r, c) not in visited:
-                    seen = set()
-                    self.dfs(grid, r, c, direction, visited, seen, None)
-                    if True in seen:
-                        return True
-                    
-        return False
-    
-    
-    def dfs(self, grid, r, c, direction, visited, seen, parent):
+        m, n = len(grid), len(grid[0])
+        coloring_grid = [[0 for i in range(n)] for j in range(m)]
+        ans = [False]
         
-        visited.add((r, c))
-        seen.add((r, c))
+        for r in range(m):
+            for c in range(n):
+                if coloring_grid[r][c] == 0:
+                    if ans[0] == True:return True
+                    self.dfs(direction, grid, r, c, coloring_grid, (-1, -1), ans)
+        
+        return ans[0]
+    
+    def dfs(self, direction, grid, r, c, coloring_grid, parent, ans):
+        
+        coloring_grid[r][c] = 1
+        node = grid[r][c]
         for x, y in direction:
+            
             nr, nc = r+x, c+y
-            if (nr, nc) in seen and (nr, nc) != parent:
-                seen.add(True)
+            if self.in_bound(nr, nc, grid) and coloring_grid[nr][nc] == 1 and (nr, nc) != parent and node == grid[nr][nc]:
+                ans[0] = True
                 break
                 
-            if self.is_inbound(nr, nc, grid) and grid[nr][nc] == grid[r][c] and (nr, nc) != parent:
-                self.dfs(grid, nr, nc, direction, visited, seen, (r, c))
-
-
-    def is_inbound(self, r, c, grid):
-        return 0 <= r < len(grid) and 0 <= c < len(grid[0])
+            if self.in_bound(nr, nc, grid) and coloring_grid[nr][nc] == 0 and (nr, nc) != parent and node == grid[nr][nc]:
+                self.dfs(direction, grid, nr, nc, coloring_grid, (r, c), ans)
+        
+        coloring_grid[r][c] = 2
+        
+        
+    def in_bound(self, r, c, grid):
+        return 0 <= r < len(grid) and 0 <= c < len(grid[0]) 
+            
+    
+    
+    
