9e5dcae4cba9ad2525216d3092b41d69ef6a578e
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-05-18 09:35:32 -0800

    Time: 1626 ms (74.18%), Space: 61.1 MB (81.29%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -4,49 +4,45 @@ class Solution:
         
         ob = Unionfind(n)
         for _from, _to, weight in roads:
-            ob.union(_from-1, _to-1)
-        
-        ans = inf
-        for _from, _, weight in roads:
-            if ob.isconnected(_from-1, n-1):
-                ans = min(ans, weight)
-                
-        return ans
+            ob.union(_from, _to, weight)
+            
+        return ob.getAnswer(ob.find(n))
 
 class Unionfind:
     def __init__(self, n):
         
-        self.root = [i for i in range(n)]
-        self.rank = [0 for _ in range(n)]
-        self.min = [inf for _ in range(n)]
+        self.root = [i for i in range(n+1)]
+        self.rank = [0 for _ in range(n+1)]
+        self.min = [inf for _ in range(n+1)]
         
-    def union(self, x, y):
+    def union(self, x, y, weight):
         
         rootx = self.find(x)
         rooty = self.find(y)
         
         if rootx == rooty:
+            self.min[rootx] = min(self.min[rootx], weight)
             return
         
         if self.rank[rootx] == self.rank[rooty]:
-            # self.root[rooty] = rootx//''
             self.rank[rootx] += 1
         
         if self.rank[rootx] > self.rank[rooty]:
+            self.min[rootx] = min(self.min[rootx], self.min[rooty], weight) 
             self.root[rooty] = rootx
         else:
+            self.min[rooty] = min(self.min[rootx], self.min[rooty], weight) 
             self.root[rootx] = rooty
-            
         
     def find(self, x):
-        
         if self.root[x] != x:
             self.root[x] = self.find(self.root[x])
             
         return self.root[x]
     
-    def isconnected(self, _from, n):
-        return self.find(_from) == self.find(n)
+    def getAnswer(self, n):
+        return self.min[n]
+    
     
     
     
