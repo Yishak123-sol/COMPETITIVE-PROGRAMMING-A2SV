57a44b11586769f241c67713e3fba5c9d3ed9c34
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-05-19 10:38:26 -0800

    Time: 68 ms (8.98%), Space: 16.5 MB (25.35%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,47 +1,34 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         
-        equivalent = UnionFind()
-        for eq in equations:
-            if eq[1] == "=":
-                equivalent.union(ord(eq[0])-97, ord(eq[3])-97)
-                
-        for eq in equations:
-            if eq[1] == "!":
-                if equivalent.find(ord(eq[0])-97) == equivalent.find(ord(eq[3])-97):
+        connected = defaultdict(list)
+        for equ in equations:
+            if equ[1] == '=':
+                connected[equ[0]].append(equ[3])
+                connected[equ[3]].append(equ[0])
+    
+        for equ in equations:
+            if equ[1] == '!':
+                if self.bfs(equ[0], connected, equ[3]) or self.bfs(equ[3], connected, equ[0]):
                     return False
+                
         return True
     
-class UnionFind():
-    
-    def __init__(self):
-        self.root = [i for i in range(26)]
-        self.rank = [0 for _ in range(26)]
-    
-    def union(self, x, y):
+    def bfs(self, node, connected, tar):
         
-        rootx = self.find(x)
-        rooty = self.find(y)
-        
-        if rootx == rooty:
-            return 
-        
-      
-        if self.rank[rootx] == self.rank[rooty]:
-            self.rank[rootx] += 1
-        
-        if self.rank[rootx] >= self.rank[rooty]:
-            self.root[rooty] = rootx
-        else:
-            self.root[rootx] = rooty
-        
-    def find(self, x):
-        
-        if self.root[x] != x:
-            self.root[x] = self.find(self.root[x])
+        visited = set()
+        queue = deque()
+        queue.append(node)
+        while queue:
             
-        return self.root[x]
+            node = queue.popleft()
+            if node == tar:return True
+            visited.add(node)
             
-
+            for neighbor in connected[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                if neighbor == tar:return True
+                
+        return False
             
-        
