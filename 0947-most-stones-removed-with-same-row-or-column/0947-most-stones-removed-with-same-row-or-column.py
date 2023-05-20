1e56f91b7c5beae94627360d161185a7b5192b63
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-05-20 09:21:13 -0800

    Time: 1578 ms (17.12%), Space: 16.9 MB (36.41%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,5 +1,6 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
+        
         n = len(stones)
         uf = UnionFind(n)
         
@@ -12,24 +13,31 @@ class Solution:
         
 class UnionFind:
     def __init__(self, n):
+        
         self.parent = list(range(n))
         self.size = [1] * n
 
     def find(self, x):
+        
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
+            
         return self.parent[x]
 
     def union(self, x, y):
+        
         root_x, root_y = self.find(x), self.find(y)
         if root_x == root_y:
             return False
-        if self.size[root_x] < self.size[root_y]:
-            root_x, root_y = root_y, root_x
-            
-        self.parent[root_y] = root_x
-        self.size[root_x] += self.size[root_y]
         
+        if self.size[root_x] == self.size[root_y]:
+            self.size[root_x] += 1
+        
+        if self.size[root_x] >= self.size[root_y]:
+            self.parent[root_y] = root_x
+        else:
+            self.parent[root_x] = root_y
+            
         return True
 
 
