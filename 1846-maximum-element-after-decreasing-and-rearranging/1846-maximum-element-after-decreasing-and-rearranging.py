8923a1f8b1bd772ea3a4fe1b501abdf5c09a4b8c
Yishak solomon <113773177+Yishak123-sol@users.noreply.github.com>
2023-05-26 03:09:01 -0800

    Time: 534 ms (26.19%), Space: 26.2 MB (57.14%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -1,13 +1,10 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
           
-        arr.sort()
-        if arr[0] != 1:
-            arr.pop()
-            arr.insert(0, 1)
-            
+        arr.sort()    
         for i in range(1, len(arr)):
             if abs(arr[i] - arr[i-1]) >= 1:
                 arr[i] = arr[i-1] + 1
-            
-        return max(arr)
+        
+        maxval = max(arr)
+        return maxval if maxval <= len(arr) else len(arr)
